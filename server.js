359250e2ebabefcf031152cc4683cdc1a385ce61
Vaibhav4556 <burdev741@gmail.com>
2022-09-09 11:45:19 +0530

    res

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "production")
 
   app.get('*' , (req , res)=>{
 
-    res.sendFile(path.resolve(__dirname , '/pizza_house/build/index.html'))
+    res.sendFile(path.resolve(__dirname , 'pizza_house/build/index.html'))
   })
 }
 
